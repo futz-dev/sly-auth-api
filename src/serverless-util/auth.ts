bf0de8a79a0ba17a49afea10205220bef2ad4c35
Christian Nuss <christian.nuss@gmail.com>
2021-05-26 17:27:31 -0700

    Cache Authorize Lookup Results

--- src/serverless-util/auth.ts
+++ src/serverless-util/auth.ts
@@ -1,8 +1,27 @@
 import { HttpError } from '@scaffoldly/serverless-util';
 import axios from 'axios';
 import { JWT } from 'jose';
+import crypto from 'crypto';
+import moment, { Moment } from 'moment';
 import { extractAuthorization, extractToken } from './http';
 import { DecodedJwtPayload, HttpRequest } from './interfaces';
+
+const authCache: { [key: string]: { payload: DecodedJwtPayload; expires: Moment } } = {};
+
+const createCacheKey = (
+  token: string,
+  request: HttpRequest,
+): { key: string; method: string; path: string } => {
+  const key = {
+    token,
+    method: request.method,
+    path: request.path,
+  };
+  const sha = crypto.createHash('sha1');
+  sha.update(JSON.stringify(key));
+
+  return { key: sha.digest('base64'), method: key.method, path: key.path };
+};
 
 export async function authorize(
   request: HttpRequest,
@@ -27,6 +46,18 @@ export async function authorize(
   const decoded = JWT.decode(token) as DecodedJwtPayload;
   if (!decoded) {
     throw new Error('Unable to decode token');
+  }
+
+  const cacheKey = createCacheKey(token, request);
+
+  if (authCache[cacheKey.key]) {
+    const { expires, payload } = authCache[cacheKey.key];
+    if (moment().isBefore(expires)) {
+      console.log(
+        `Returning cached payload for ${payload.aud} (expires: ${expires}; cacheKey: ${cacheKey})`,
+      );
+      return payload;
+    }
   }
 
   const { authorizeUrl } = decoded;
@@ -54,5 +85,9 @@ export async function authorize(
     throw new Error('Unauthorized');
   }
 
-  return payload;
+  const ret = payload as DecodedJwtPayload;
+
+  authCache[cacheKey.key] = { payload, expires: moment(ret.exp * 1000) };
+
+  return authCache[cacheKey.key].payload;
 }
